7ffe87223a9b8a9415da06def0412a8034722c58
Insu-qg <quent.grange@gmail.com>
2024-03-22 09:04:35 +0100

    init pwa

---
+++ vite.config.ts
@@ -0,0 +1,61 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react(), VitePWA(
+    {
+      workbox: {
+        runtimeCaching: [
+          {
+            urlPattern: 'http://localhost:3000',
+            handler: 'CacheFirst',
+            options: {
+              cacheName: 'currentDate',
+              expiration: {
+                maxEntries: 10,
+                maxAgeSeconds: 60 * 60 * 24 * 365 // <== 365 days
+              },
+              cacheableResponse: {
+                statuses: [0, 200]
+              }
+            }
+          }
+        ]
+      },
+      manifest:{
+
+        theme_color: "#f69435",
+        background_color: "#7df635",
+        display: "standalone",
+        scope: "/",
+        start_url: "/",
+        name: "Controller",
+        short_name: "Con",
+        icons: [
+          {
+            src: "/icon-192x192.png",
+            sizes: "192x192",
+            type: "image/png"
+          },
+          {
+            src: "/icon-256x256.png",
+            sizes: "256x256",
+            type: "image/png"
+          },
+          {
+            src: "/icon-384x384.png",
+            sizes: "384x384",
+            type: "image/png"
+          },
+          {
+            src: "/icon-512x512.png",
+            sizes: "512x512",
+            type: "image/png"
+          }
+        ]
+      }
+      })
+    ],
+  })
