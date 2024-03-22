516e71d7480133eb15c5638bbad583c911942431
Insu-qg <quent.grange@gmail.com>
2024-03-22 09:36:50 +0100

    maj notif

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,56 @@
+import { SetStateAction, useEffect, useState } from 'react';
 import './App.css'
 
 function App() {
+  const [supportsPWA, setSupportsPWA] = useState(true);
+  const [promptInstall, setPromptInstall] = useState(null);
+
+  useEffect(() => {
+    // Enregistrement du service worker
+    if ('serviceWorker' in navigator) {
+      window.addEventListener('load', async () => {
+        try {
+          const registration = await navigator.serviceWorker.register('/service-worker.js');
+          console.log('Service Worker enregistré : ', registration);
+        } catch (error) {
+          console.error('Échec de l enregistrement du Service Worker : ', error);
+        }
+      });
+    }
+
+    // Demande d'autorisation de notification
+    if ('Notification' in window) {
+      Notification.requestPermission().then(permission => {
+        if (permission !== 'granted') {
+          console.warn('Permission de notification non accordée.');
+        }
+      });
+    }
+  }, []);
+
+  function handleSendNotification(): void {
+    if ('Notification' in window && Notification.permission === 'granted') {
+      new Notification('Titre de la notification', {
+        body: 'Corps de la notification',
+        icon: 'chemin/vers/une/image.png' // Facultatif : chemin de l'icône pour la notification
+      });
+    }
+  }
+
 
   return (
-    <>
-      <div></div>
-    </>
-  )
+    <div>
+      {supportsPWA ? 
+        <button onClick={() => handleSendNotification()}>
+          Installer l'application
+        </button>
+      :
+      <div>
+        If you see this it means you can't install PWA try again using chrome
+      </div>
+      }
+    </div>
+  );
 }
 
 export default App
